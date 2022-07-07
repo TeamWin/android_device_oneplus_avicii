055c5c979c99c94d23c9d3c51348a1b62b700aba
rany <ranygh@riseup.net>
2022-07-07 19:15:16 +0300

    avicii: Initial dummy tree

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_avicii.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_avicii-user \
+    omni_avicii-userdebug \
+    omni_avicii-eng
