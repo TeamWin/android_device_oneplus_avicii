68f481a458d6d10c2536a7ec8c06410ba22aab76
rany <ranygh@riseup.net>
2022-07-08 08:25:16 +0300

    avicii: remove unused flags

--- device.mk
+++ device.mk
@@ -50,9 +50,6 @@ PRODUCT_HOST_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
-# Shipping API level
-PRODUCT_SHIPPING_API_LEVEL := 29
-
 # Encryption
 PRODUCT_PACKAGES += \
     qcom_decrypt \
@@ -70,7 +67,4 @@ RECOVERY_LIBRARY_SOURCE_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so \
     $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@1.0.so \
     $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@2.0.so \
-    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/libdisplayconfig.qti.so \
-
-#PRODUCT_COPY_FILES += \
-#    $(OUT_DIR)/target/product/$(PRODUCT_RELEASE_NAME)/obj/SHARED_LIBRARIES/libandroidicu_intermediates/libandroidicu.so:$(TARGET_COPY_OUT_RECOVERY)/root/system/lib64/libandroidicu.so
+    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/libdisplayconfig.qti.so
