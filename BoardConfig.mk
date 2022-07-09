725d934b7b06650e0fa641358d96323506a81701
rany <ranygh@riseup.net>
2022-07-09 15:27:00 +0300

    avicii: BoardConfig: remove TW_EXCLUDE_APEX

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -124,7 +124,6 @@ RECOVERY_BINARY_SOURCE_FILES += $(TARGET_OUT_EXECUTABLES)/strace
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /config/usb_gadget/g1/functions/mass_storage.0/lun.%d/file
 TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel0-backlight/brightness"
 TW_DEFAULT_BRIGHTNESS := 200
-TW_EXCLUDE_APEX := true
 TW_EXCLUDE_DEFAULT_USB_INIT := true
 TW_EXTRA_LANGUAGES := true
 TW_HAS_EDL_MODE := true
