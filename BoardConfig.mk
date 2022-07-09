7639e97aaca7f5192d93463d7c55a01fd5af8ce4
rany <ranygh@riseup.net>
2022-07-09 15:26:38 +0300

    avicii: BoardConfig: sort some sections so it looks nicer

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -121,35 +121,35 @@ TARGET_RECOVERY_DEVICE_MODULES += strace
 RECOVERY_BINARY_SOURCE_FILES += $(TARGET_OUT_EXECUTABLES)/strace
 
 # TWRP Specific Build Flags
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /config/usb_gadget/g1/functions/mass_storage.0/lun.%d/file
 TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel0-backlight/brightness"
 TW_DEFAULT_BRIGHTNESS := 200
-TW_MAX_BRIGHTNESS := 1023
-TW_THEME := portrait_hdpi
-TW_EXTRA_LANGUAGES := true
 TW_EXCLUDE_APEX := true
 TW_EXCLUDE_DEFAULT_USB_INIT := true
-TW_NO_CPU_TEMP := true
-TW_NO_SCREEN_BLANK := true
+TW_EXTRA_LANGUAGES := true
+TW_HAS_EDL_MODE := true
 TW_INCLUDE_NTFS_3G := true
 TW_INCLUDE_REPACKTOOLS := true
 TW_INCLUDE_RESETPROP := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
-TW_USE_FSCRYPT_POLICY := 1
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /config/usb_gadget/g1/functions/mass_storage.0/lun.%d/file
-TW_HAS_EDL_MODE := true
+TW_MAX_BRIGHTNESS := 1023
+TW_NO_CPU_TEMP := true
+TW_NO_SCREEN_BLANK := true
 TW_OVERRIDE_SYSTEM_PROPS := \
     "ro.build.date.utc;ro.bootimage.build.date.utc=ro.build.date.utc;ro.odm.build.date.utc=ro.build.date.utc;ro.product.build.date.utc=ro.build.date.utc;ro.system.build.date.utc=ro.build.date.utc;ro.system_ext.build.date.utc=ro.build.date.utc;ro.vendor.build.date.utc=ro.build.date.utc;ro.build.product;ro.build.fingerprint=ro.system.build.fingerprint;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name"
-TW_Y_OFFSET := 128
+TW_THEME := portrait_hdpi
 TW_H_OFFSET := -128
+TW_Y_OFFSET := 128
 
 # TWRP Decryption
+BOARD_USES_METADATA_PARTITION := true
+BOARD_USES_QCOM_FBE_DECRYPTION := true
+PLATFORM_SECURITY_PATCH := 2127-12-31
 PLATFORM_VERSION := 127
 PLATFORM_VERSION_LAST_STABLE := $(PLATFORM_VERSION)
 TW_INCLUDE_CRYPTO := true
 TW_INCLUDE_CRYPTO_FBE := true
-BOARD_USES_METADATA_PARTITION := true
-BOARD_USES_QCOM_FBE_DECRYPTION := true
-PLATFORM_SECURITY_PATCH := 2127-12-31
+TW_USE_FSCRYPT_POLICY := 1
 VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 
 # Extras
